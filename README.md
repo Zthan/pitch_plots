a05eeb93b8301b467b1a413b208e4b0d27aad584
Zthan <38104256+Zthan@users.noreply.github.com>
2025-03-11 02:28:56 +0000

    Framework for script

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-import streamlit as st
-from pybaseball.plotting import plot_strike_zone
-from pybaseball import statcast_pitcher
-import pandas as pd
 
+
+ 
--- pitch_plots.py
+++ pitch_plots.py
@@ -1,1 +1,26 @@
+import streamlit as st
+from pybaseball.plotting import plot_strike_zone
+from pybaseball import statcast_pitcher
+import pandas as pd
+
+# Title and description
+st.title("Field Manager Pitch Plots")
+st.write(
+    "Choose an MLB pitcher and game and see their pitches plotted for that game. "
+)
+
+# Get pitchers for this year and last 3 years
+
+
+# Get either game dates or game matchups, or both in list
+
+
+# Read in files
+
+
+
+# Make selection boxes
+
+
+# Create pitch plot, capture image, and display
 
