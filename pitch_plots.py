1b628b07a3dd96440bc9cc497048015928f49e70
Zthan <38104256+Zthan@users.noreply.github.com>
2025-03-11 03:46:09 +0000

    some comments getting ready for things

--- pitch_plots.py
+++ pitch_plots.py
@@ -8,6 +8,20 @@ st.title("Field Manager Pitch Plots")
 st.write(
     "Choose an MLB pitcher and game and see their pitches plotted for that game. "
 )
+# Notes on what the arguments for plot strike zone are
+#plot_strike_zone(data: pd.DataFrame, title: str = '', colorby: str = 'pitch_type', 
+#       legend_title: str = '', annotation: str = 'pitch_type', axis: Optional[axes.Axes] = None) -> axes.Axes:
+
+#Arguments
+#data: StatCast pandas.DataFrame of StatCast pitcher data
+#title: (str), default = '' Optional: Title of plot
+#colorby: (str), default = 'pitch_type', Optional: Which category to color the mark with. 'pitch_type', 
+#       'pitcher', 'description' or a column within data
+#legend_title: (str), default = based on colorby, Optional: Title for the legend
+#annotation: (str), default = 'pitch_type', Optional: What to annotate in the marker. 'pitch_type', 
+#       'release_speed', 'effective_speed', 'launch_speed', or something else in the data
+#axis: (matplotlib.axis.Axes), default = None, Optional: Axes to plot the strike zone on. If None, a new Axes will be created
+
 
 # Get pitchers for this year and last 3 years
 
