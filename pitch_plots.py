1cacc458f760925dd95fec22ccb491ef4f4124f4
Zthan <38104256+Zthan@users.noreply.github.com>
2025-03-12 11:08:31 +0000

    spraychart working

--- pitch_plots.py
+++ pitch_plots.py
@@ -26,14 +26,25 @@ st.write(
 # Get pitchers for this year and last 3 years
 data = pd.read_csv('https://raw.githubusercontent.com/Zthan/pitch_plots/refs/heads/main/pitch_plot_player_list.csv')
 
-# Create a dictionary of player names and their MLBAM IDs
-player_dict = data.apply(lambda row: (f"{row['name_first']} {row['name_last']}", row['key_mlbam']), axis=1).to_dict()
+# Get full names of players
+#full_names = data.apply(lambda row: f"{row['name_first']} {row['name_last']}", axis=1).tolist()
+#full_names.sort()
+#full_names = [s.title() for s in full_names]
 
+data['Full Name'] = data['name_first'] + ' ' + data['name_last']
+data['Full Name'] = data['Full Name'].str.title()
+#full_names = pitching_data.apply(lambda row: f"{row['name_first']} {row['name_last']}", axis=1)#.tolist()
+
+# Create a dictionary of player names and their MLBAM IDs
+# df.set_index('Key')['Value'].to_dict()
+# need to make full names a single column first then do the second player dict below
+#player_dict = data.apply(lambda row: (f"{row['Full Name']}", row['key_mlbam']), axis=1).to_dict()
+player_dict = data.set_index('Full Name')['key_mlbam'].to_dict()
 
 # Get full names of players
 full_names = list(player_dict.keys())
 full_names.sort()
-full_names = [s.title() for s in full_names]
+#full_names = [s.title() for s in full_names]
 
 # Default player selection
 default_index = full_names.index('Spencer Schwellenbach')
@@ -48,38 +59,31 @@ mlbam_id = player_dict[entered_name]
 # Default player selection
 #default_index = full_names.index('Spencer Schwellenbach')
 #entered_name = st.selectbox("Pick an MLB Player.", full_names, index=default_index)
-name_first = entered_name.split(' ')[0]
-name_last = entered_name.split(' ')[1]
+#name_first = entered_name.split(' ')[0]
+#name_last = entered_name.split(' ')[1]
 
 # Get either game dates or game matchups, or both in list
 game_list = pd.read_csv('https://raw.githubusercontent.com/Zthan/pitch_plots/refs/heads/main/pitch_plot_game_list.csv')
 # Filter game_list based on mlbam id of entered name
+game_list['option'] = game_list['game_date'] + ' - ' + game_list['Matchup']
 filtered_game_list = game_list[game_list['pitcher'] == mlbam_id]
 
 # Ensure the filtered_game_list has the necessary columns
-if 'game_date' in filtered_game_list.columns and 'matchup' in filtered_game_list.columns:
+#if 'game_date' in filtered_game_list.columns and 'matchup' in filtered_game_list.columns:
     # Create a list of formatted game dates and matchups
-    game_options = filtered_game_list.apply(lambda row: f"{row['game_date']} - {row['matchup']}", axis=1).tolist()
-    game_options.sort()
-else:
-    game_options = []
+#    game_options = filtered_game_list.apply(lambda row: f"{row['game_date']} - {row['matchup']}", axis=1).tolist()
+#    game_options.sort()
+#else:
+#    game_options = []
+#game_options = filtered_game_list.apply(lambda row: f"{row['game_date']} - {row['matchup']}", axis=1).tolist()
+entered_game = st.selectbox("Pick a game to plot.", filtered_game_list['option'])
 
-entered_game = st.selectbox("Pick a game to plot.", game_options)
-
-# Filter game_list based on entered name
-#game_list = game_list[game_list['player_name'] == entered_name]
-#game_list = game_list['game_date'].tolist()
-#game_list.sort()
-#game_list = [str(s) for s in game_list]
-
-#entered_game = st.selectbox("Pick a game to plot.", game_list)
-
-# Read in files
-
-
-
-# Make selection boxes
-
+# make colorby options selectable
+colorby_options = ['pitch_type', 'description', 'release_speed', 'launch_speed', 'hit_distance_sc']
+entered_colorby = st.selectbox("Pick a colorby option.", colorby_options)
 
 # Create pitch plot, capture image, and display
-
+game_date = entered_game.split(' - ')[0]
+pitcher_data = statcast_pitcher(game_date, game_date, mlbam_id)
+plot_img = plot_strike_zone(pitcher_data, title=f"{entered_name} - {entered_game}", colorby='pitch_type', legend_title='pitch_type', annotation=entered_colorby)
+st.pyplot(plot_img.figure)
